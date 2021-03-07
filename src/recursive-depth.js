ead7384289a019451e1c7d9887ba29e4e2d2455d
Andrey Kononov <50419270+fastilov3@users.noreply.github.com>
2021-03-07 11:40:11 +0300

    Update recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -8,3 +8,23 @@ module.exports = class DepthCalculator {
     }
   }
 };
+
+// 'use strict'
+// module.exports = class DepthCalculator {
+//     constructor() {
+//         this.currentDepth = 0;
+//         this.depth = 1;
+//     }
+
+//     calculateDepth(arr) {
+//         let array = arr.filter(el => Array.isArray(el));
+//         if (array.length === 0) {
+//             this.currentDepth = this.depth;
+//             this.depth = 1;
+//             return this.currentDepth;
+//         } else {
+//             this.depth++;
+//             return this.calculateDepth([].concat(...array));
+//         }
+//     }
+// };
