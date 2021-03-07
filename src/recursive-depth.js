17e9f1513da350c41b22cc2160e65688c851cad8
Andrey Kononov <50419270+fastilov3@users.noreply.github.com>
2021-03-07 11:35:14 +0300

    Update recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,10 @@
 const CustomError = require("../extensions/custom-error");
-'use strict'
 module.exports = class DepthCalculator {
-    constructor() {
-        this.currentDepth = 0;
-        this.depth = 1;
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return 0;
+    else if (arr.length === 0) return 1;
+    else {
+      return 1 + Math.max(...arr.map(this.calculateDepth.bind(this)));
     }
-
-    calculateDepth(arr) {
-        let array = arr.filter(el => Array.isArray(el));
-        if (array.length === 0) {
-            this.currentDepth = this.depth;
-            this.depth = 1;
-            return this.currentDepth;
-        } else {
-            this.depth++;
-            return this.calculateDepth([].concat(...array));
-        }
-    }
+  }
 };
