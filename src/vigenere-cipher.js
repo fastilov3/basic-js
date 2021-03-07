141f43dc416da0ca2c1ccd5f6567d0d921e2bced
Andrey Kononov <50419270+fastilov3@users.noreply.github.com>
2021-03-07 11:37:11 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,57 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor(mode = true) {
-    this.directMode = mode;
-  }
-
-  encrypt(sourceMessage, key) {
-    if (arguments.length < 2) throw new Error("Needs two arguments");
-
-    const res = [];
-    sourceMessage = sourceMessage.toUpperCase();
-    key = key.toUpperCase();
-    const sourceLength = sourceMessage.length;
-    const keyLength = key.length;
-
-    for (let i = 0, j = 0; i < sourceLength; i++) {
-      if (!/[A-Z]/.test(sourceMessage[i])) res.push(sourceMessage[i]);
-      else {
-        const M = sourceMessage[i].codePointAt() - 65;
-        const K = key[j].codePointAt() - 65;
-        const C = 65 + ((M + K) % 26);
-        res.push(String.fromCodePoint(C));
-
-        j = (j + 1) % keyLength;
-      }
+    constructor(type = true) {
+        this.type = type;
     }
 
-    return this.directMode ? res.join("") : res.reverse().join("");
-  }
+    encrypt(message, key) {
+        if (arguments.length < 2) throw new Error("Needs two arguments");
 
-  decrypt(encryptedMessage, key) {
-    if (arguments.length < 2) throw new Error("Needs two arguments");
+        const res = [];
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        const sourceLength = message.length;
+        const keyLength = key.length;
 
-    const res = [];
-    key = key.toUpperCase();
-    const encryptedLength = encryptedMessage.length;
-    const keyLength = key.length;
+        for (let i = 0, j = 0; i < sourceLength; i++) {
+            if (!/[A-Z]/.test(message[i])) res.push(message[i]);
+            else {
+                const M = message[i].codePointAt() - 65;
+                const K = key[j].codePointAt() - 65;
+                const C = 65 + ((M + K) % 26);
+                res.push(String.fromCodePoint(C));
 
-    for (let i = 0, j = 0; i < encryptedLength; i++) {
-      if (!/[A-Z]/.test(encryptedMessage[i])) res.push(encryptedMessage[i]);
-      else {
-        const C = encryptedMessage[i].codePointAt() - 65;
-        const K = key[j].codePointAt() - 65;
-        const M = 65 + ((C - K + 26) % 26);
-        res.push(String.fromCodePoint(M));
+                j = (j + 1) % keyLength;
+            }
+        }
 
-        j = (j + 1) % keyLength;
-      }
+        return this.directMode ? res.join("") : res.reverse().join("");
     }
 
-    return this.directMode ? res.join("") : res.reverse().join("");
-  }
+    decrypt(message, key) {
+        if (arguments.length < 2) throw new Error("Needs two arguments");
+
+        const res = [];
+        key = key.toUpperCase();
+        const encryptedLength = message.length;
+        const keyLength = key.length;
+
+        for (let i = 0, j = 0; i < encryptedLength; i++) {
+            if (!/[A-Z]/.test(message[i])) res.push(message[i]);
+            else {
+                const C = message[i].codePointAt() - 65;
+                const K = key[j].codePointAt() - 65;
+                const M = 65 + ((C - K + 26) % 26);
+                res.push(String.fromCodePoint(M));
+
+                j = (j + 1) % keyLength;
+            }
+        }
+
+        return this.type ? res.join("") : res.reverse().join("");
+    }
 }
-
 
 module.exports = VigenereCipheringMachine;
